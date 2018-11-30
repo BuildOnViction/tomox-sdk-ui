db11740d23035a891d45aef7824b7b4af398e102
Hoang Trinh <hoang.trinhj@gmail.com>
2018-11-30 17:43:36 +0700

    Update TrezorSigner class

--- src/store/services/signer/trezor.js
+++ src/store/services/signer/trezor.js
@@ -1,5 +1,5 @@
 import { ethers, providers } from "ethers";
-import TrezorConnect from "../device/trezor/trezor-connect";
+import TrezorConnect from "trezor-connect";
 
 import { NETWORK_URL } from "../../../config/url";
 import { addMethodsToSigner } from "./index";
@@ -14,44 +14,56 @@ export class TrezorSigner extends ethers.Signer {
             chainId: networkId,
             name: undefined
         });
-        window.signer = { instance: this.provider.getSigner(), type: "wallet" };
+        window.signer = { instance: this, type: "hardwareWallet" };
         addMethodsToSigner(this);
     }
 
     getPublicKey = (path = defaultDPath) => {
-        return new Promise((resolve, reject) => {
-            TrezorConnect.getXPubKey(path, result => {
-                if (result.success) {
-                    result.dPath = path;
-                    resolve(result);
-                } else {
-                    var err = "Cannot connect to trezor";
-                    if (result.toString() === "Error: Not a valid path.") {
-                        err = "This path not supported by Trezor";
-                    }
-                    reject(err);
-                }
+        return new Promise(async (resolve, reject) => {
+            let result = await TrezorConnect.getPublicKey({
+                path
             });
+            if (result.success) {
+                resolve(result.payload);
+            } else {
+                console.log(result);
+                reject(result.payload.error);
+            }
         });
     };
 
     getAddress = (path = defaultDPath) => {
-        return new Promise((resolve, reject) => {
-            TrezorConnect.getAddress(path, result => {
-                if (result.success) {
-                    resolve(result.payload.address);
-                } else {
-                    var err = "Cannot connect to trezor";
-                    if (result.toString() === "Error: Not a valid path.") {
-                        err = "This path not supported by Trezor";
-                    }
-                    reject(err);
-                }
+        return new Promise(async (resolve, reject) => {
+            let result = await TrezorConnect.getAddress({
+                path,
+                coin: "eth"
             });
+            if (result.success) {
+                resolve(result.payload.address);
+            } else {
+                console.log(result);
+                reject(result.payload.error);
+            }
         });
     };
 
-    signMessage = message => {};
+    signMessage = async message => {
+        return new Promise(async (resolve, reject) => {
+            let result = await TrezorConnect.signMessage({
+                path: defaultDPath,
+                message
+            });
+            console.log(result);
+            if (result.success) {
+                resolve(result.payload.signature);
+            } else {
+                console.error("Error:", result.payload.error); // error message
+                reject(result.payload.error);
+            }
+        });
+    };
 
-    sendTransaction = transaction => {};
+    sendTransaction = transaction => {
+        console.log("Trezor sendTransaction");
+    };
 }
