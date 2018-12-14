37a100ff25237bdfc5f7cd1a57d0f14d0067ad67
tu@agiletech.vn <tubackkhoa@gmail.com>
2018-12-14 15:39:18 +0700

    standardize ws url

--- src/config/environment.js
+++ src/config/environment.js
@@ -7,15 +7,25 @@ let ENGINE_HTTP_URL,
 
 const env = window.env || process.env
 
+const WS_PROTOCOL = window.location.protocol.replace('http', 'ws')
+const standardizeWSProtocol = url => {
+  if (!url.startsWith('ws')) {
+    url = `${WS_PROTOCOL}//${window.location.hostname}/${url}`
+  }
+  return url
+}
+
 if (env) {
   // ENGINE_HTTP_URL = 'http://engine'
   // ENGINE_WS_URL = 'ws://engine'
   // ETHEREUM_NODE_HTTP_URL = 'http://ethereum/'
   // ETHEREUM_NODE_WS_URL = 'ws://ethereum/ws/'
   ENGINE_HTTP_URL = env.REACT_APP_ENGINE_HTTP_URL
-  ENGINE_WS_URL = env.REACT_APP_ENGINE_WS_URL
+  ENGINE_WS_URL = standardizeWSProtocol(env.REACT_APP_ENGINE_WS_URL)
   ETHEREUM_NODE_HTTP_URL = env.REACT_APP_ETHEREUM_NODE_HTTP_URL
-  ETHEREUM_NODE_WS_URL = env.REACT_APP_ETHEREUM_NODE_WS_URL
+  ETHEREUM_NODE_WS_URL = standardizeWSProtocol(
+    env.REACT_APP_ETHEREUM_NODE_WS_URL
+  )
   DEFAULT_NETWORK_ID = env.REACT_APP_DEFAULT_NETWORK_ID || 'default'
   BZZ_URL = env.REACT_APP_ENGINE_BZZ_URL
 }
@@ -26,5 +36,6 @@ export {
   ETHEREUM_NODE_HTTP_URL,
   ETHEREUM_NODE_WS_URL,
   DEFAULT_NETWORK_ID,
-  BZZ_URL
+  BZZ_URL,
+  WS_PROTOCOL,
 }
