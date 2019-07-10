0e45606ea1ac59e22c11986cf20b6d4aa28479b9
thanhvk <thanhvk.w3s@gmail.com>
2019-07-10 14:32:17 +0700

    Rename logo image

--- src/components/Common/TomoXLogo.jsx
+++ src/components/Common/TomoXLogo.jsx
@@ -1,6 +1,6 @@
 //@flow
 import React from 'react'
-import tomoXLogo from '../../assets/tomoLogo.png'
+import tomoXLogo from '../../assets/logo.png'
 
 const TomoXLogo = (props: { height: number, width: number, alt: string }) => {
   const { height, width, alt } = props
