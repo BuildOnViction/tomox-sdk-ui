f4752bd0039b6a40cb20fbd9264e0b3ae773eb26
thanhvk <thanhvk.w3s@gmail.com>
2019-09-05 15:49:29 +0700

    Remove LandingPage

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,7 +3,6 @@ import { Route, Redirect, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import Layout from './Layout'
-// import LandingPage from './LandingPage'
 import LoginPage from './LoginPage'
 import CreateWalletPage from './CreateWalletPage'
 import WalletPage from './WalletPage'
@@ -12,7 +11,6 @@ import LogoutPage from './LogoutPage'
 import TradingPage from './TradingPage'
 import MarketsPage from './MarketsPage'
 
-// import SocketController from '../components/SocketController'
 import { ConnectedRouter } from 'connected-react-router'
 import history from '../store/history'
 import '../styles/css/index.css'
@@ -23,20 +21,18 @@ class App extends React.PureComponent {
   render() {
     return (
       <ConnectedRouter history={history}>
-        {/* <SocketController> */}
-          <Layout>
-            <Switch>
-              <Route exact path="/" render={() => <Redirect to="/markets" />} />
-              <Route path="/unlock" component={LoginPage} />
-              <Route path="/wallet" component={WalletPage} />
-              <Route path="/markets" component={MarketsPage} />
-              <Route path="/trade/:pair?" component={TradingPage} />
-              <Route path="/settings" component={SettingsPage} />
-              <Route path="/logout" component={LogoutPage} />
-              <Route path="/create" component={CreateWalletPage} />
-            </Switch>
-          </Layout>
-        {/* </SocketController> */}
+        <Layout>
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/markets" />} />
+            <Route path="/unlock" component={LoginPage} />
+            <Route path="/wallet" component={WalletPage} />
+            <Route path="/markets" component={MarketsPage} />
+            <Route path="/trade/:pair?" component={TradingPage} />
+            <Route path="/settings" component={SettingsPage} />
+            <Route path="/logout" component={LogoutPage} />
+            <Route path="/create" component={CreateWalletPage} />
+          </Switch>
+        </Layout>
       </ConnectedRouter>
     )
   }
