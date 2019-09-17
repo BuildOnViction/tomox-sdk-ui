4c65b6c73ab8592b11c6a0a453dd099c58289b4a
thanhvk <thanhvk.w3s@gmail.com>
2019-09-17 17:55:35 +0700

    Add urls of tomoscan and tomo tokens repo to config

--- src/config/environment.js
+++ src/config/environment.js
@@ -2,12 +2,12 @@ let ENGINE_HTTP_URL,
   ENGINE_WS_URL,
   TOMOCHAIN_NODE_HTTP_URL,
   TOMOCHAIN_NODE_WS_URL,
+  TOMOSCAN_URL,
+  TOMOTOKENS_URL,
   DEFAULT_NETWORK_ID,
   REACT_APP_DEX_VERSION
 
 const env = window.env || process.env
-
-// console.log(env)
 
 const WS_PROTOCOL = window.location.protocol.replace('http', 'ws')
 const standardizeWSProtocol = url => {
@@ -21,16 +21,14 @@ const standardizeWSProtocol = url => {
 }
 
 if (env) {
-  // ENGINE_HTTP_URL = 'http://engine'
-  // ENGINE_WS_URL = 'ws://engine'
-  // TOMOCHAIN_NODE_HTTP_URL = 'http://ethereum/'
-  // TOMOCHAIN_NODE_WS_URL = 'ws://ethereum/ws/'
   ENGINE_HTTP_URL = env.REACT_APP_ENGINE_HTTP_URL
   ENGINE_WS_URL = standardizeWSProtocol(env.REACT_APP_ENGINE_WS_URL)
   TOMOCHAIN_NODE_HTTP_URL = env.REACT_APP_TOMOCHAIN_NODE_HTTP_URL
   TOMOCHAIN_NODE_WS_URL = standardizeWSProtocol(
     env.REACT_APP_TOMOCHAIN_NODE_WS_URL
   )
+  TOMOSCAN_URL = env.REACT_APP_TOMOSCAN_URL
+  TOMOTOKENS_URL = env.REACT_APP_TOMOTOKENS_URL
   DEFAULT_NETWORK_ID = env.REACT_APP_DEFAULT_NETWORK_ID || 'default'
   REACT_APP_DEX_VERSION = env.REACT_APP_DEX_VERSION || 'ALPHA'
 }
@@ -40,6 +38,8 @@ export {
   ENGINE_WS_URL,
   TOMOCHAIN_NODE_HTTP_URL,
   TOMOCHAIN_NODE_WS_URL,
+  TOMOSCAN_URL,
+  TOMOTOKENS_URL,
   DEFAULT_NETWORK_ID,
   WS_PROTOCOL,
   REACT_APP_DEX_VERSION,
