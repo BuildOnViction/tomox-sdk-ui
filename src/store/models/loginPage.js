c25488f4b69e7cf2e6874d5c23b7eee7d6b6354a
thanhvk <thanhvk.w3s@gmail.com>
2019-10-29 09:55:23 +0700

    Remove notification 'Signed in with Wallet'

--- src/store/models/loginPage.js
+++ src/store/models/loginPage.js
@@ -9,8 +9,6 @@ import {
   createMetamaskSigner,
 } from '../services/signer'
 
-// import { LedgerSigner } from '../services/signer/ledger';
-
 import type
  { State, ThunkAction } from '../../types'
 
@@ -22,19 +20,19 @@ export default function loginPageSelector(state: State) {
     loading: getLoginPageDomain(state).isLoading(),
     error: getLoginPageDomain(state).getError(),
     isSelectAddressModalOpen: getLoginPageDomain(state).isSelectAddressModalOpen(),
-    getPublicKeyData: () => getLoginPageDomain(state).getPublicKeyData()
-  };
+    getPublicKeyData: () => getLoginPageDomain(state).getPublicKeyData(),
+  }
 }
 
 export function loginWithMetamask(): ThunkAction {
   return async (dispatch, getState) => {
     try {
-      dispatch(actionCreators.requestLogin());
+      dispatch(actionCreators.requestLogin())
       if (typeof window.web3 === 'undefined')
-        throw new Error('Metamask not installed');
+        throw new Error('Metamask not installed')
       if (typeof window.web3.eth.defaultAccount === 'undefined')
-        throw new Error('Metamask account locked');
-      let { address } = await createMetamaskSigner();
+        throw new Error('Metamask account locked')
+      const { address } = await createMetamaskSigner()
 
       // Check account exist on backend yet? 
       // Create account if not yet for get balance of account from backend
@@ -42,25 +40,20 @@ export function loginWithMetamask(): ThunkAction {
       const accountInfo = await fetchAccountInfo(address)
       if (!accountInfo) { await createAccount(address) }
 
-      dispatch(actionCreators.loginWithMetamask(address));
-      dispatch(
-        notifierActionCreators.addSuccessNotification({
-          message: 'Signed in with Wallet',
-        })
-      );
+      dispatch(actionCreators.loginWithMetamask(address))
     } catch (e) {
       if (e.message === 'Metamask account locked')
-        return dispatch(actionCreators.loginError('Metamask account locked'));
+        return dispatch(actionCreators.loginError('Metamask account locked'))
       if (e.message === 'Metamask not installed')
-        return dispatch(actionCreators.loginError('Metamask not installed'));
-      if (process.env.NODE_ENV !== 'test') console.log(e);
+        return dispatch(actionCreators.loginError('Metamask not installed'))
+      if (process.env.NODE_ENV !== 'test') console.log(e)
 
       dispatch(
         notifierActionCreators.addNotification({ message: 'Login error ' })
-      );
-      dispatch(actionCreators.loginError(e.message));
+      )
+      dispatch(actionCreators.loginError(e.message))
     }
-  };
+  }
 }
 
 export function loginWithWallet(wallet, password): ThunkAction {
@@ -80,14 +73,8 @@ export function loginWithWallet(wallet, password): ThunkAction {
 
       dispatch(actionCreators.createWallet(wallet.address))
       dispatch(actionCreators.loginWithWallet(address, privateKeyEncrypted))
-      dispatch(
-        notifierActionCreators.addSuccessNotification({
-          message: `Signed in with ${address}`,
-        })
-      )
     } catch (e) {
       console.log(e)
-      // dispatch(actionCreators.loginError('Could not authenticate wallet'));
       dispatch(
         notifierActionCreators.addNotification({ message: 'Login Error' })
       )
@@ -99,40 +86,40 @@ export function loginWithWallet(wallet, password): ThunkAction {
 export function getTrezorPublicKey(deviceService: any, path: string): ThunkAction {
   return async (dispatch, getState) => {
     try {
-      dispatch(actionCreators.requestLogin());
-      let result = await deviceService.getPublicKey(path);
+      dispatch(actionCreators.requestLogin())
+      const result = await deviceService.getPublicKey(path)
 
-      dispatch(actionCreators.getPublicKey(result));
-      dispatch(actionCreators.toggleSelectAddressModal(true));
+      dispatch(actionCreators.getPublicKey(result))
+      dispatch(actionCreators.toggleSelectAddressModal(true))
     } catch (e) {
       dispatch(
         notifierActionCreators.addNotification({ message: 'Login error' })
-      );
-      dispatch(actionCreators.loginError(e.message));
+      )
+      dispatch(actionCreators.loginError(e.message))
     }
-  };
+  }
 }
 
 export function closeSelectAddressModal(): ThunkAction {
   return async (dispatch, getState) => {
     try {
-      dispatch(actionCreators.toggleSelectAddressModal(false));
+      dispatch(actionCreators.toggleSelectAddressModal(false))
     } catch (e) {
       dispatch(
         notifierActionCreators.addNotification({ message: 'Login error' })
-      );
-      dispatch(actionCreators.loginError(e.message));
+      )
+      dispatch(actionCreators.loginError(e.message))
     }
-  };
+  }
 }
 
 export function getBalance(address: string): ThunkAction {
   return async (dispatch, getState) => {
     try {
-      let balance = await getSigner().provider.getBalance(address);
-      return balance;
+      const balance = await getSigner().provider.getBalance(address)
+      return balance
     } catch (e) {
-      return 0;
+      return 0
     }
   }
 }
@@ -140,14 +127,15 @@ export function getBalance(address: string): ThunkAction {
 export function loginWithTrezorWallet(data: Object): ThunkAction {
   return async (dispatch, getState) => {
     try {
+      // Check account exist on backend yet? 
+      // Create account if not yet for get balance of account from backend
+      // Remove when connect direct to TomoX
+      const accountInfo = await fetchAccountInfo(data.address)
+      if (!accountInfo) { await createAccount(data.address) }
+
       dispatch(actionCreators.toggleSelectAddressModal(false))
       dispatch(actionCreators.requestLogin())
       dispatch(actionCreators.loginWithTrezorWallet(data.address))
-      dispatch(
-        notifierActionCreators.addSuccessNotification({
-          message: 'Signed in with Trezor wallet',
-        })
-      )
     } catch (e) {
       dispatch(
         notifierActionCreators.addNotification({ message: 'Login error' })
@@ -167,13 +155,7 @@ export function loginWithLedgerWallet(address): ThunkAction {
       if (!accountInfo) { await createAccount(address.addressString) }
 
       window.signer.instance.setAddress(address)
-
       dispatch(actionCreators.loginWithLedgerWallet(address.addressString))
-      dispatch(
-        notifierActionCreators.addSuccessNotification({
-          message: 'Signed in with Ledger wallet',
-        })
-      )
     } catch (e) {
       dispatch(
         notifierActionCreators.addNotification({ message: 'Login error' })
