3698963d87ddc2028e8d11cd803217d93dfdc08f
thanhvk <thanhvk.w3s@gmail.com>
2019-11-25 17:43:56 +0700

    fix deptChart.test.jsx

--- src/components/DepthChart/depthChart.test.jsx
+++ src/components/DepthChart/depthChart.test.jsx
@@ -1,29 +1,12 @@
 import React from 'react'
-import { mount } from 'enzyme'
-import OrderBook from './OrderBook'
+import { shallow } from 'enzyme'
+import DepthChart from './DepthChart';
 
-describe('Component methods', () => {
-  let wrapper, instance
-  const currentPair = {
-    pair: 'DAI_WETH',
-    baseTokenSymbol: 'DAI',
-    quoteTokenSymbol: 'WETH',
-    baseTokenAddress: '0xc838efcb6512a2ca12027ebcdf9e1fc5e4ff7ee3',
-    quoteTokenAddress: '0xe8e84ee367bc63ddb38d3d01bccef106c194dc47',
+it('renders without crashing', () => {
+  const props = {
+    bids: [],
+    asks: [],
   }
 
-  beforeEach(() => {
-    wrapper = mount(<OrderBook asks={[]} bids={[]} currentPair={currentPair} />)
-    instance = wrapper.instance()
-  })
-
-  it('changeTab modifies selectedTabId state', () => {
-    instance.changeTab('depth-chart')
-    expect(wrapper.state('selectedTabId')).toBe('depth-chart')
-  })
-
-  it('toggleCollapse modifies isOpen state', () => {
-    instance.toggleCollapse()
-    expect(wrapper.state('isOpen')).toBe(false)
-  })
+  shallow(<DepthChart {...props} />)
 })
