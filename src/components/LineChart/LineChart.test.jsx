6802a43c4917cee3c813bb4a3781fbddb191b59b
thanhvk <thanhvk.w3s@gmail.com>
2019-11-25 17:35:43 +0700

    remove LineChart.test.jsx

--- src/components/LineChart/LineChart.test.jsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-// import { shallow } from 'enzyme'
-// import LineChart from './LineChart'
-
-// it('renders without crashing', () => {
-//   const wrapper = shallow(
-//     <LineChart />)
-// })
