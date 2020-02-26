c3b200ff77323e28aa9072be7f44abd5cebccfa5
thanhvk <thanhvk.w3s@gmail.com>
2020-02-26 15:03:55 +0700

    fix turkey language not work

--- src/locales/index.js
+++ src/locales/index.js
@@ -3,11 +3,12 @@
 import { addLocaleData } from 'react-intl'
 import locale_en from 'react-intl/locale-data/en'
 import locale_vi from 'react-intl/locale-data/vi'
+import locale_tr from 'react-intl/locale-data/tr'
 import messages_en from './langs/en.json'
 import messages_vi from './langs/vi.json'
 import messages_tr from './langs/tr.json'
 
-addLocaleData([...locale_en, ...locale_vi])
+addLocaleData([...locale_en, ...locale_vi, ...locale_tr])
 
 export const messsages = {
     'en': messages_en,
