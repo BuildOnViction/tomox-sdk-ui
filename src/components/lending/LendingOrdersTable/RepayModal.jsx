a5ff0930f390b39fff3b57023b2c9e635d865e70
thanhvk <thanhvk.w3s@gmail.com>
2020-03-25 12:33:46 +0700

    style repay modal

--- src/components/lending/LendingOrdersTable/RepayModal.jsx
+++ src/components/lending/LendingOrdersTable/RepayModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from '@blueprintjs/core'
+import styled from 'styled-components'
 
 import Modal from '../../Modal'
 
@@ -11,18 +12,51 @@ export default function RepayModal({
 }) {
     return (
         <Modal onClose={() => onClose(false)} {...rest}>
-            <p>Note: you are repaying your borrowing before the pay-off date, it could make you have to imposed more 1&#37; fee</p>
-            <p>Total repay amount: USDT</p>
-            <div>
-                <Button 
+            <Typo>Note: you are repaying your borrowing before the pay-off date, it could make you have to imposed more 1&#37; fee</Typo>
+            <Typo>Total repay amount: __ USDT</Typo>
+            <ButtonGroup>
+                <CancelButton 
                     text="No, let me pay later"
                     onClick={() => onClose(false)}
                 />
-                <Button 
+                <RepayButton 
                     onClick={() => onRepay(hash)} 
                     text="Yes, I want to repay you" 
                 />
-            </div>
+            </ButtonGroup>
         </Modal>
     )
 }
+
+const ButtonGroup = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const BaseButton = styled(Button)`
+    width: 47%;
+    padding-top: 10px !important;
+    padding-bottom: 10px !important;
+    color: #fff !important;
+    box-shadow: none !important;
+    background-image: none !important;
+`
+
+const CancelButton = styled(BaseButton)`
+    background-color: #3f4a67 !important;
+    &:hover {
+        background-color: #333c54 !important;
+    }
+`
+
+const RepayButton = styled(BaseButton)`
+    color: #18454e !important
+    background-color: #00e8b5 !important;
+    &:hover {
+        background-color: #00c59a !important;
+    }
+`
+
+const Typo = styled.p`
+    margin-bottom: 25px;
+`
