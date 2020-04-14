b1ab3b2d4d4096a3ec47ce9461bc669c498e4c71
thanhvk <thanhvk.w3s@gmail.com>
2020-04-14 11:37:47 +0700

    fix change 24h in lending markets page alway is 0

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -409,6 +409,7 @@ export const parseLendingPairsData = (pairsData, tokens) => {
       lendingToken: item.lendingID.lendingToken.toLowerCase(),
       term: item.lendingID.term,
       volume: item.volume ? parseLendingAmount(item.volume, decimals) : 0,
+      change: computeChange(item.open, item.close),
     }
   })
 
