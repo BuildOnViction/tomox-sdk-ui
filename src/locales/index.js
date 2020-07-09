9f791969c89872821c0f7b074ccc078b96eb2f99
thanhvk <thanhvk.w3s@gmail.com>
2020-07-09 10:55:05 +0700

    add chinese language #282

--- src/locales/index.js
+++ src/locales/index.js
@@ -7,9 +7,11 @@ import locale_tr from 'react-intl/locale-data/tr'
 import locale_fr from 'react-intl/locale-data/fr'
 import locale_nl from 'react-intl/locale-data/nl'
 import locale_ja from 'react-intl/locale-data/ja'
+import locale_zh from 'react-intl/locale-data/zh'
 
 import { enUS, vi, fr, nl, ja  } from 'date-fns/locale'
 import tr from 'date-fns/locale/tr'
+import zh from 'date-fns/locale/zh-CN'
 
 import messages_en from './langs/en.json'
 import messages_vi from './langs/vi.json'
@@ -17,8 +19,9 @@ import messages_tr from './langs/tr.json'
 import messages_fr from './langs/fr.json'
 import messages_nl from './langs/nl.json'
 import messages_ja from './langs/ja.json'
+import messages_zh from './langs/zh.json'
 
-addLocaleData([...locale_en, ...locale_vi, ...locale_tr, ...locale_fr, ...locale_nl, ...locale_ja])
+addLocaleData([...locale_en, ...locale_vi, ...locale_tr, ...locale_fr, ...locale_nl, ...locale_ja, ...locale_zh])
 
 export const messsages = {
     'en': messages_en,
@@ -27,6 +30,7 @@ export const messsages = {
     'fr': messages_fr,
     'nl': messages_nl,
     'ja': messages_ja,
+    'zh': messages_zh,
 }
 
 export const locales = {
@@ -36,6 +40,7 @@ export const locales = {
     'fr': 'Français',
     'nl': 'Nederlands',
     'ja': '日本語',
+    'zh': '简体中文',
 }
 
 export const LANGUAGES = {
@@ -45,4 +50,5 @@ export const LANGUAGES = {
     fr,
     nl,
     ja,
+    zh,
 }
